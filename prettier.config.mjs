12e550af0579e903e0e7681d2dc3ab68f0b710a4
Arvind <pingarvindforquestions@gmail.com>
2024-06-19 10:15:45 +0000

    feat: added prettier to the project

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+export const trailingComma = "es5";
+export const tabWidth = 2;
+export const semi = true;
+export const singleQuote = true;
+export const jsxSingleQuote = true;
+export const plugins = ["prettier-plugin-tailwindcss"];
+export const importOrderParserPlugins = [
+  "typescript",
+  "jsx",
+  "decorators",
+  "decorators-legacy",
+];
